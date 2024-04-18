61af2d34c4533c2a40608d2b1311a120bc0a1793
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-04-18 10:44:04 -0400

    use namespaced key

--- services/StorageService.ts
+++ services/StorageService.ts
@@ -6,13 +6,18 @@ enum StorageKeys {
   savedRolls = 'savedRolls',
 }
 
+const key = (key: StorageKeys) => `@randsum:${key}`
+
 async function getSavedRolls() {
-  const rawTotals = await AsyncStorage.getItem(StorageKeys.savedRolls)
+  const rawTotals = await AsyncStorage.getItem(key(StorageKeys.savedRolls))
   return rawTotals ? JSON.parse(rawTotals) : []
 }
 
 async function setSavedRolls(savedRolls: Roll[]) {
-  await AsyncStorage.setItem(StorageKeys.savedRolls, JSON.stringify(savedRolls))
+  await AsyncStorage.setItem(
+    key(StorageKeys.savedRolls),
+    JSON.stringify(savedRolls)
+  )
 }
 
 export default {
